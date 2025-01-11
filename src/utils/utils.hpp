2354fc0809712c5823d6da401856b1fbc93e7218
Gledson Paiva <gleds.hpaiva2@gmail.com>
2025-01-11 18:46:26 -0300

    Add files via upload

---
+++ src/utils/utils.hpp
@@ -0,0 +1,10 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <string>
+#include <vector>
+
+// Função para dividir uma entrada em palavras
+std::vector<std::string> dividirEntrada(const std::string& entrada);
+
+#endif
